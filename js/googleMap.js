71e45a1bfe10ccd424f1fcae8349bf50d7d560a1
TrevorStrom <xboxdoctor133@gmail.com>
2017-04-27 15:49:22 -0500

    Added Location Content
    Maps and hours added

---
+++ js/googleMap.js
@@ -0,0 +1,13 @@
+function initMap() {
+    var uluru = {lat: 44.789666, lng: -91.460420};
+    var map = new google.maps.Map(document.getElementById('eauClaireMap'), {zoom: 16, center: uluru});
+    var marker = new google.maps.Marker({
+        position: uluru, 
+        map: map});
+    
+    var uluru2 = {lat: 44.936879, lng: -91.393144};
+    var map2 = new google.maps.Map(document.getElementById('chippewaFallsMap'), {zoom: 16, center: uluru});
+    var marke2r = new google.maps.Marker({
+        position: uluru, 
+        map: map2});
+}
